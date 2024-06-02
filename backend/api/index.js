f757b192743f89701f157cbc8a40f8dc314bffad
inamnurulf <aam.inam7310@gmail.com>
2024-06-02 13:52:21 +0700

    deployment 2

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,40 @@
-import app from '../index'
+require("dotenv").config();
 
-export default app
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const ConnectToMongodb = require("../src/lib/db");
+
+const articleRoute = require("../src/routes/articleRoutes")
+const landFillRoute = require("../src/routes/landFillRoutes")
+
+
+const app = express();
+const frontendDomain = process.env.DOMAIN_FRONTEND || "http://localhost:3000";
+app.use(
+  cors({
+    origin : frontendDomain,
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json({ limit: "1000mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "1000mb" }));
+
+ConnectToMongodb();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Backend on Vercel"));
+
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
+  });
+
+  app.use("/article", articleRoute);
+  app.use("/landfill", landFillRoute);
+
+  export default app
+
+  
